9fd1a513eb17c805ae3cd34c54e8f664442aa5aa
Satoshi Takeda <takeda@mediba.jp>
2018-05-13 17:30:06 +0900

    :construction: to Slack WebHook URL

--- src/lambda/post.js
+++ src/lambda/post.js
@@ -1,43 +1,63 @@
-const https = require('https')
-const url = require('url')
-const slackUrl = process.env.SLACK_WEBHOOK_URL
-const slackReqOptions = url.parse(slackUrl)
-slackReqOptions.method = 'POST'
-slackReqOptions.headers = {'Content-Type': 'application/json'}
+import fetch from 'node-fetch'
 
-exports.handler = async (event, context) => {
-  const { url, title } = event.queryStringParameters
+const slackURL = process.env.SLACK_WEBHOOK_URL
 
-  if (url) {
-    const req = https.request(slackReqOptions, res => {
-      if (res.statusCode === 200) {
-        console.log('posted to slack')
-      } else {
-        // err
-      }
+exports.handler = (event, context, callback) => {
+  // Bad Method
+  if (event.httpMethod !== 'POST') {
+    return callback(null, {
+      statusCode: 410,
+      body: 'Unsupported Request Method'
     })
-    // error handle
-    req.on('error', e => {
-      console.log('problem with request: ' + e.message)
-      // err
-    })
-    // request send
-    req.write(JSON.stringify({
-      username: 'mjs',
-      icon_emoji: ':javascript:',
-      text: `I\'m just published. <${decodeURI(url)}|${title || 'detail'}>\nDEBUG:\nurl=>${url}\ntitle=>${title}`
-    }))
-    // request end
-    req.end()
-    // response
+  }
+  // Bad Content-Type
+  if (event.headers['content-type'] !== 'application/x-www-form-urlencoded') {
     return {
-      statusCode: 200,
-      body: `${title} - ${url} done.\nSLACK_WEBHOOK_URL: ${slackUrl}`
-    }
-  } else {
-    return {
-      statusCode: 403
+      statusCode: 422,
+      body: 'Invalid content type. Only application/x-www-form-urlencoded is supported'
     }
   }
+  try {
+    const { url, title } = parseQueryString(event.body)
+    if (!url && !title) {
+      callback(null, {
+        statusCode: 400,
+        body: 'Bad request'
+      })
+    }
+    fetch(slackURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: 'mjs',
+        icon_emoji: ':javascript:',
+        text: `I\'m just published. <${decodeURIComponent(url)}|${title || 'detail'}>`
+      })
+    }).then(()=>{
+      callback(null, {
+        statusCode: 204
+      })
+    })
+  } catch (e) {
+    callback(null, {
+      statusCode: 500,
+      body: 'Internal Server Error: ' + e
+    })
+  } 
+}
+
+function parseQueryString(queryString) {
+  return queryString
+    .split('&')
+    .map(param => {
+      const [key, value] = param.split('=')
+      return { key, value }
+    })
+    .reduce((params, { key, value }) => {
+      params[key] = value
+      return params
+    }, {})
 }
 
